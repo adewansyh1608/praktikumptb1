587c2736ad558729552babba170a8a3eb1d7ed2c
adewansyh1608 <2311523001_andre@student.unand.ac.id>
2025-10-07 20:24:43 +0700

    siap juga

--- app/src/main/java/com/example/shoppinglist/component/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/component/ShoppingList.kt
@@ -1,5 +1,5 @@
 package com.example.shoppinglist.component
-
+//
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
